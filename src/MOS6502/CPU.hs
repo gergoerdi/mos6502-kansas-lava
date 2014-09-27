8400d8c6e144b1d6eb7b7d78bfda39a758a9bfdd
Dr. ERDI Gergo <gergo@erdi.hu>
2014-09-27 14:59:17 +0800

    Implement new opcodes: indirect LDA/STA, CMP, INC, BNE/BEQ

--- src/MOS6502/CPU.hs
+++ src/MOS6502/CPU.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE ScopedTypeVariables, TypeFamilies #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE RecordWildCards #-}
 module MOS6502.CPU where
 
@@ -8,10 +8,9 @@ import MOS6502.Utils
 
 import Language.KansasLava
 import Data.Sized.Ix
+import Data.Sized.Matrix
+import qualified Data.Sized.Matrix as Matrix
 import Data.Bits
-
-import Data.Bimap (Bimap)
-import qualified Data.Bimap as Bimap
 
 data CPUIn clk = CPUIn
     { cpuMemR :: Signal clk Byte
@@ -41,19 +40,22 @@ data CPUDebug clk = CPUDebug
     , cpuOp :: Signal clk Opcode
     }
 
-data State = Init
+data State = Halt
+           | Init
            | FetchVector1
            | FetchVector2
            | Fetch1
            | Fetch2
            | Fetch3
-           | WaitMem
-           | Halt
+           | Indirect1
+           | Indirect2
+           | WaitRead
+           | WaitWrite
            deriving (Show, Eq, Enum, Bounded)
-type StateSize = X8
+type StateSize = X11
 
 instance Rep State where
-    type W State = X3 -- W StateSize
+    type W State = X4 -- W StateSize
     newtype X State = XState{ unXState :: Maybe State }
 
     unX = unXState
@@ -69,45 +71,89 @@ instance Rep State where
 
     repType _ = repType (Witness :: Witness StateSize)
 
+data Indirect s clk = ReadIndirect (Signal clk Byte -> RTL s clk ())
+                    | WriteIndirect (RTL s clk (Signal clk Byte))
+
+newtype Modify s clk = Modify (Signal clk Byte -> RTL s clk (Signal clk Byte))
+
 data Microcode s clk = Opcode0 (RTL s clk ())
-                     | Opcode1 (Signal clk Byte -> (RTL s clk ()))
-                     | Opcode2 (Signal clk Addr -> (RTL s clk ()))
+                     | Opcode1 (Signal clk Byte -> RTL s clk ())
+                     | Opcode2 (Signal clk Addr -> RTL s clk ())
+                     | DirectZP (Signal clk Byte -> Signal clk Byte) (Signal clk Byte -> RTL s clk ())
+                     | IndirectZP (Signal clk Byte -> Signal clk Byte) (Indirect s clk)
+                     | ModifyZP (Signal clk Byte -> Signal clk Byte) (Modify s clk)
+                     -- | IndirectAddr (Signal clk Addr -> Signal clk Addr) (Signal clk Byte -> RTL s clk ())
                      | Jam
 
-cpu :: forall clk. (Clock clk) => CPUIn clk -> (CPUOut clk, CPUDebug clk)
+bitsToByte :: (Clock clk)
+           => Matrix X8 (Signal clk Bool)
+           -> Signal clk Byte
+bitsToByte = bitwise . packMatrix
+
+cpu :: (Clock clk) => CPUIn clk -> (CPUOut clk, CPUDebug clk)
 cpu CPUIn{..} = runRTL $ do
     -- State
     s <- newReg Init
     rOp <- newReg BRK
     rArgLo <- newReg 0x00
+    let argAddr = (unsigned cpuMemR `shiftL` 8) .|. unsigned (reg rArgLo)
 
     -- Registers
     rA <- newReg 0x00
     rX <- newReg 0x00
     rY <- newReg 0x00
     rSP <- newReg 0x00
-    rP <- newReg 0x00
     rPC <- newReg 0x0000 -- To be filled in by Init
+
+    -- Flags
+    fC <- newReg False
+    fZ <- newReg False
+    fI <- newReg False
+    fD <- newReg False
+    fB <- newReg False
+    fV <- newReg False
+    fN <- newReg False
+
+    let flags = bitsToByte . Matrix.fromList $
+                map reg [fC, fZ, fI, fD, fB, fV, fN] ++ [low]
 
     rNextA <- newReg 0x0000
     rNextW <- newReg Nothing
 
+    let setA v = do
+            rA := v
+            fZ := v .==. 0
+            fN := v .>=. 0x80
+
     let write addr val = do
             rNextA := addr
             rNextW := enabledS val
-            s := pureS WaitMem
+            s := pureS WaitWrite
         delay1 = return () -- TODO
 
     let op LDA_Imm = Opcode1 $ \imm -> do
-            rA := imm
+            setA imm
+        op LDA_ZP = DirectZP id $ \v -> do
+            setA v
+        op LDA_Ind_X = IndirectZP (+ reg rX) $ ReadIndirect $ \v -> do
+            setA v
+
         op STA_Abs = Opcode2 $ \addr -> do
             write addr (reg rA)
+        op STA_ZP = Opcode1 $ \zp -> do
+            write (unsigned zp) (reg rA)
+        op STA_ZP_X = Opcode1 $ \zp -> do
+            write (unsigned (zp + reg rX)) (reg rA)
         op STA_Abs_X = Opcode2 $ \addr -> do
             let addr' = addr + unsigned (reg rX)
             write addr' (reg rA)
             delay1
+        op STA_Ind_X = IndirectZP (+ reg rX) $ WriteIndirect $ do
+            return $ reg rA
+
         op LDX_Imm = Opcode1 $ \imm -> do
             rX := imm
+
         op INX = Opcode0 $ do
             rX := reg rX + 1
             delay1
@@ -117,8 +163,27 @@ cpu CPUIn{..} = runRTL $ do
         op TXA = Opcode0 $ do
             rX := reg rA
             delay1
+
+        op CMP_Imm = Opcode1 $ \imm -> do
+            fC := reg rA .>=. imm
+            fZ := reg rA .==. imm
+            fN := reg rA .>=. 0x80
+
+        op INC_ZP = ModifyZP id $ Modify $ \v -> do
+            let v' = v + 1
+            fZ := v' .==. 0
+            fN := v' .>=. 0x80
+            return $ v + 1
+
         op JMP_Abs = Opcode2 $ \addr -> do
             rPC := addr
+
+        op BNE = Opcode1 $ \offset -> do
+            WHEN (bitNot $ reg fZ) $ do
+                rPC := reg rPC + 1 + unsigned offset
+        op BEQ = Opcode1 $ \offset -> do
+            WHEN (reg fZ) $ do
+                rPC := reg rPC + 1 + unsigned offset
 
         op _ = Jam
 
@@ -150,8 +215,16 @@ cpu CPUIn{..} = runRTL $ do
           Fetch2 -> do
               switch (reg rOp) $ \k -> case op k of
                   Opcode1 act -> do
-                      let arg = cpuMemR
-                      act arg
+                      act cpuMemR
+                  DirectZP toZP _ -> do
+                      rNextA := unsigned $ toZP cpuMemR
+                      s := pureS Indirect1
+                  IndirectZP toZP _ -> do
+                      rNextA := unsigned $ toZP cpuMemR
+                      s := pureS Indirect1
+                  ModifyZP toZP _ -> do
+                      rNextA := unsigned $ toZP cpuMemR
+                      s := pureS Indirect1
                   Opcode2 _ -> do
                       rArgLo := cpuMemR
                       s := pureS Fetch3
@@ -163,14 +236,49 @@ cpu CPUIn{..} = runRTL $ do
           Fetch3 -> do
               switch (reg rOp) $ \k -> case op k of
                   Opcode2 act -> do
-                      let arg = (unsigned cpuMemR `shiftL` 8) .|. unsigned (reg rArgLo)
-                      act arg
+                      act argAddr
                   _ -> do
                       s := pureS Halt
               rPC := reg rPC + 1
               rNextA := var rPC
               s := pureS Fetch1
-          WaitMem -> do
+          Indirect1 -> do
+              switch (reg rOp) $ \k -> case op k of
+                  DirectZP _ act -> do
+                      act cpuMemR
+                      rNextA := reg rPC
+                      s := pureS Fetch1
+                  IndirectZP{} -> do
+                      rArgLo := cpuMemR
+                      rNextA := reg rNextA + 1
+                      s := pureS Indirect2
+                  ModifyZP _ (Modify modify) -> do
+                      v <- modify cpuMemR
+                      rNextW := enabledS v
+                      s := pureS WaitWrite
+                  _ -> return ()
+              s := pureS Halt
+          Indirect2 -> do
+              switch (reg rOp) $ \k -> case op k of
+                  IndirectZP _ (ReadIndirect _) -> do
+                      rNextA := argAddr
+                      s := pureS WaitRead
+                  IndirectZP _ (WriteIndirect act) -> do
+                      rNextA := argAddr
+                      v <- act
+                      rNextW := enabledS v
+                      s := pureS WaitWrite
+                  _ -> do
+                      s := pureS Halt
+          WaitRead -> do
+              switch (reg rOp) $ \k -> case op k of
+                  IndirectZP _ (ReadIndirect act) -> do
+                      act cpuMemR
+                      rNextA := reg rPC
+                      s := pureS Fetch1
+                  _ -> do
+                      s := pureS Halt
+          WaitWrite -> do
               rNextW := disabledS
               rNextA := reg rPC
               s := pureS Fetch1
@@ -188,7 +296,7 @@ cpu CPUIn{..} = runRTL $ do
         cpuX = reg rX
         cpuY = reg rY
         cpuSP = reg rSP
-        cpuP = reg rP
+        cpuP = flags
         cpuPC = reg rPC
 
     return (CPUOut{..}, CPUDebug{..})
--- src/MOS6502/Opcodes.hs
+++ src/MOS6502/Opcodes.hs
@@ -12,8 +12,12 @@ import qualified Data.Bimap as Bimap
 import Data.Maybe
 
 data Opcode = LDA_Imm
+            | LDA_ZP
             | LDA_Abs_X
+            | LDA_Ind_X
+            | LDA_Ind_Y
             | STA_ZP
+            | STA_ZP_X
             | STA_Abs
             | STA_Abs_X
             | STA_Ind_X
@@ -26,24 +30,31 @@ data Opcode = LDA_Imm
             | TXA
             | TAY
             | TYA
+            | INC_ZP
             | DEC_ZP
             | LDX_Imm
             | LDX_ZP
             | LDY_Abs_X
             | STX_ZP
             | JMP_Abs
+            | BEQ
             | BNE
             | CLC
             | ADC_ZP
             | ASL_A
+            | CMP_Imm
             | BRK
             deriving (Eq, Ord, Bounded, Enum)
 
 opcodes :: Bimap Opcode Byte
 opcodes = Bimap.fromList
           [ (LDA_Imm,   0xA9)
+          , (LDA_ZP,    0xA5)
           , (LDA_Abs_X, 0xBD)
+          , (LDA_Ind_X, 0xA1)
+          , (LDA_Ind_Y, 0xB1)
           , (STA_ZP,    0x85)
+          , (STA_ZP_X,  0x95)
           , (STA_Abs,   0x8D)
           , (STA_Abs_X, 0x9D)
           , (STA_Ind_X, 0x81)
@@ -56,16 +67,19 @@ opcodes = Bimap.fromList
           , (TXA,       0xBA)
           , (TAY,       0xA8)
           , (TYA,       0x98)
+          , (INC_ZP,    0xE6)
           , (DEC_ZP,    0xC6)
           , (LDX_Imm,   0xA2)
           , (LDX_ZP,    0xA6)
           , (LDY_Abs_X, 0xBC)
           , (STX_ZP,    0x44)
           , (JMP_Abs,   0x4C)
+          , (BEQ,       0xF0)
           , (BNE,       0xD0)
           , (CLC,       0x18)
           , (ADC_ZP,    0x75)
           , (ASL_A,     0x0A)
+          , (CMP_Imm,   0xC9)
           , (BRK,       0x00)
           ]
 
